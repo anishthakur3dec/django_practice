7f91d436a8c02c770114c81731c106503b6c12fd
Anish Kumar Thakur <anishthakur3dec@gmail.com>
2024-01-10 09:39:48 +0530

    Updated commit    Changes to be committed: 	modified:   Myapp/Myapp/__pycache__/urls.cpython-311.pyc 	modified:   Myapp/Myapp/urls.py 	modified:   Myapp/db.sqlite3 Changes not staged for commit: modified:   Myapp/base/__pycache__/views.cpython-311.pyc modified:   Myapp/base/views.py modified:   Myapp/templates/index.html

--- Myapp/Myapp/urls.py
+++ Myapp/Myapp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+admin.site.site_header = "Profile Admin"
+admin.site.index_title = "Welcome to Anish Profile"
+admin.site.site_title = "Anish's Description"
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
